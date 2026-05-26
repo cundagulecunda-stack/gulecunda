989ff40938fe947448b89b111e2e9e0f510ed151
onur.kocmanoglu@icloud.com <4505096aA.a>
2026-05-26 17:17:19 +0300

    Update env configuration and deployment settings

--- next.config.js
+++ next.config.js
@@ -4,15 +4,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
